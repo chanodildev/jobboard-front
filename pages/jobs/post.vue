612e0db9a3319027e2208597855d5b60ed949030
= <=>
2020-04-17 18:58:02 +0100

    post jobs working

---
+++ pages/jobs/post.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h1>Post job</h1>
+    <b-card class="mb-2">
+      <b-form @submit.prevent="handleSubmit">
+        <b-form-group label="title">
+          <b-form-input required v-model="form.title"></b-form-input>
+        </b-form-group>
+        <b-form-group label="description">
+          <b-form-textarea rows="5" required v-model="form.description"></b-form-textarea>
+        </b-form-group>
+        <b-form-group label="category">
+          <b-form-input required v-model="form.category"></b-form-input>
+        </b-form-group>
+        <b-form-group label="location">
+          <b-form-input required v-model="form.location"></b-form-input>
+        </b-form-group>
+        <b-button type="submit">submit</b-button>
+      </b-form>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: "auth",
+  data() {
+    return {
+      form: {}
+    };
+  },
+  methods: {
+    async handleSubmit() {
+      try {
+        const res = await this.$axios.post(`/jobs`, this.form);
+        console.log("post", res);
+        // this.$router.push("/success");
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
